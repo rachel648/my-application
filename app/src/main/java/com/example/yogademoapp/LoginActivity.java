1092126225e408311c1c3b097c861cd4acbed8f0
rachel648 <106959706+rachel648@users.noreply.github.com>
2023-05-30 23:34:47 +0300

    A lot of changes made

--- app/src/main/java/com/example/yogademoapp/LoginActivity.java
+++ app/src/main/java/com/example/yogademoapp/LoginActivity.java
@@ -3,12 +3,22 @@ package com.example.yogademoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText edUsername, edPassword;
+    Button btn;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        edUsername = findViewById(R.id.editTextTextPassword);
+        edPassword = findViewById(R.id.editTextTextPassword);
+        btn = findViewById(R.id.buttonLogin);
+        tv= findViewById(R.id.textViewRegistration);
     }
 }
