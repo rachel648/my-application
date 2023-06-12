ddbaf4f483359e3a3a7b11273b64fd5236e67d52
rachel648 <106959706+rachel648@users.noreply.github.com>
2023-06-13 01:06:19 +0300

    writable database

--- app/src/main/java/com/example/yogademoapp/Database.java
+++ app/src/main/java/com/example/yogademoapp/Database.java
@@ -1,5 +1,6 @@
 package com.example.yogademoapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -14,7 +15,7 @@ public class Database extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         String qry1 = "create table users(username text,email text,password text)";
-        sqLiteDatabase.execSQL(qry1);
+        sqLiteDatabase.execSQL(qry1); /*Creation of a table*/
     }
 
     @Override
@@ -22,6 +23,13 @@ public class Database extends SQLiteOpenHelper {
 
     }
 
-    public void registar(String username, String email, String password) {
+    public void registar(String username, String email, String password) /*Register function for Register New User*/{
+        ContentValues cv = new ContentValues();
+        cv.put("username",username);
+        cv.put("email",email);
+        cv.put("password",password);
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert("users",null,cv);
+        db.close();
     }
 }
