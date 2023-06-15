b24b8d787bcf9f74d9477ba80e6bcacd07de3310
rachel648 <106959706+rachel648@users.noreply.github.com>
2023-06-16 00:19:14 +0300

    Toast message welcome user

--- app/src/main/java/com/example/yogademoapp/HomeActivity.java
+++ app/src/main/java/com/example/yogademoapp/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.yogademoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        SharedPreferences sharedpreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedpreferences.getString("username","").toString();
+        Toast.makeText(getApplicationContext(),"Welcome" +username, Toast.LENGTH_SHORT).show();
     }
 }
