1d1cd094be382ccbde72db0171948b605d0a0be9
rachel648 <106959706+rachel648@users.noreply.github.com>
2023-06-16 00:31:13 +0300

    LOGOUT activity

--- app/src/main/java/com/example/yogademoapp/HomeActivity.java
+++ app/src/main/java/com/example/yogademoapp/HomeActivity.java
@@ -1,10 +1,13 @@
 package com.example.yogademoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
@@ -17,5 +20,17 @@ public class HomeActivity extends AppCompatActivity {
         SharedPreferences sharedpreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
         String username = sharedpreferences.getString("username","").toString();
         Toast.makeText(getApplicationContext(),"Welcome" +username, Toast.LENGTH_SHORT).show();
+
+        CardView exit = findViewById(R.id.Logout);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.clear();
+            editor.apply();
+            startActivity(new Intent(HomeActivity.this,LoginActivity.class));
+            }
+        });
+
     }
 }
