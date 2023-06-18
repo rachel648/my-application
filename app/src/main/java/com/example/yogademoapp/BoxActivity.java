c235b696a13d5c45797784ed86f524108a3504f6
rachel648 <106959706+rachel648@users.noreply.github.com>
2023-06-18 13:37:11 +0300

    Membership cardview functinality

--- app/src/main/java/com/example/yogademoapp/BoxActivity.java
+++ app/src/main/java/com/example/yogademoapp/BoxActivity.java
@@ -14,7 +14,7 @@ public class BoxActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_box);
 
-        CardView exit =findViewById(R.id.Boxback); //writinf functinally of back(boxback)
+        CardView exit = findViewById(R.id.Boxback); //writinf functinally of back(boxback)
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,16 +22,25 @@ public class BoxActivity extends AppCompatActivity {
             }
         });
 
-      CardView Class_And_Schedule =findViewById(R.id.Class_And_Schedule);
+        CardView Class_And_Schedule = findViewById(R.id.Class_And_Schedule);
         Class_And_Schedule.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent it = new Intent(BoxActivity.this,GymDetailsActivity.class);
+                Intent it = new Intent(BoxActivity.this, GymDetailsActivity.class);
                 it.putExtra("title", "Class_And_Schedule");
+                startActivity(it);
+            }
+        });
+        CardView Membership = findViewById(R.id.Membership);
+        Class_And_Schedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(BoxActivity.this, GymDetailsActivity.class);
+                it.putExtra("title", "Membership");
                 startActivity(it);
             }
         });
 
 
     }
-}
+    }
