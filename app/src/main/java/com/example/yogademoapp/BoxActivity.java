1f1c86b56ea45d2ac95fc4b6dbc9aef12ff6ad1b
rachel648 <106959706+rachel648@users.noreply.github.com>
2023-06-18 11:28:08 +0300

    Write Boxback functionally in BoxActivity

--- app/src/main/java/com/example/yogademoapp/BoxActivity.java
+++ app/src/main/java/com/example/yogademoapp/BoxActivity.java
@@ -13,7 +13,7 @@ public class BoxActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_box);
 
-        CardView exit =findViewById(R.id.Boxback);
+        CardView exit =findViewById(R.id.Boxback); //writinf functinally of back(boxback)
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
