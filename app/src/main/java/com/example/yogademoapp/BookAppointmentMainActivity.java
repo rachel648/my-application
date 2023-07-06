1e9ce154c1205833b9355a779ef61aec9d796188
rachel648 <106959706+rachel648@users.noreply.github.com>
2023-07-06 15:24:57 +0300

    Book Appointment Title

--- app/src/main/java/com/example/yogademoapp/BookAppointmentMainActivity.java
+++ app/src/main/java/com/example/yogademoapp/BookAppointmentMainActivity.java
@@ -3,9 +3,12 @@ package com.example.yogademoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BookAppointmentMainActivity extends AppCompatActivity {
-
+ EditText ed1,ed2,ed3,ed4;
+ TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
