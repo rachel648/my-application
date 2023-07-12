2c2bd5f8e0f3b8e85490821fdcf5e78a3ab31b70
rachel648 <106959706+rachel648@users.noreply.github.com>
2023-07-12 19:52:00 +0300

    MembershipProfileActivity

--- app/src/main/java/com/example/yogademoapp/MembershipProfileActivity.java
+++ app/src/main/java/com/example/yogademoapp/MembershipProfileActivity.java
@@ -3,8 +3,21 @@ package com.example.yogademoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MembershipProfileActivity extends AppCompatActivity {
+
+    Button btn;
+    EditText ed;
+    Spinner sp;
+
+    private EditText editTextName, editTextEmail, editTextPassword;
+    private Spinner spinnerMembershipType;
+    private Button buttonSubmit;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
