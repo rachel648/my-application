64f60c60133147a62158ae66f733409ece53df21
rachel648 <106959706+rachel648@users.noreply.github.com>
2023-07-29 13:07:33 +0300

    cart

--- app/src/main/java/com/example/yogademoapp/CartActivity.java
+++ app/src/main/java/com/example/yogademoapp/CartActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,7 +30,6 @@ public class CartActivity extends AppCompatActivity {
     private TimePickerDialog timePickerDialog;
 
     private Button dateButton, timeButton, btnCheckout, btnBack;
-    ;
 
 
     @Override
@@ -41,6 +42,12 @@ public class CartActivity extends AppCompatActivity {
         btnCheckout = findViewById(R.id.buttonCheckOutCart);
         btnBack = findViewById(R.id.buttonCheckoutback);
 
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username =sharedPreferences.getString("username","").toString();
+
+        Database db = new Database(getApplicationContext(),"fitness",null,1);
+        float totalamount = 0;//fetch card data and store it in Arraylist
+        ArrayList dbData = db.getCartData(username,"lab");
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,5 +55,63 @@ public class CartActivity extends AppCompatActivity {
             }
         });
 
+
+        // Call methods to initialize the date and time pickers
+        initDatePicker();
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePickerDialog.show();
+            }
+        });
+
+        initTimePicker();
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timePickerDialog.show();
+            }
+        });
     }
-}
+
+
+
+    private void initDatePicker () {
+            DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                    i1 = i1 + 1;
+                    dateButton.setText(i2 + "/" + i1 + "/" + i);
+                }
+            };
+
+            Calendar cal = Calendar.getInstance();
+            int year = cal.get(Calendar.YEAR);
+            int month = cal.get(Calendar.MONTH);
+            int day = cal.get(Calendar.DAY_OF_MONTH);
+
+            int style = AlertDialog.THEME_HOLO_DARK;
+            datePickerDialog = new DatePickerDialog(this, dateSetListener, year, month, day);
+            datePickerDialog.getDatePicker().setMinDate(cal.getTimeInMillis() + 86400000);
+        }
+
+        private void initTimePicker () {
+            TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                    timeButton.setText(i + ":" + i1);
+                }
+            };
+
+            Calendar cal = Calendar.getInstance();
+            int hrs = cal.get(Calendar.HOUR);
+            int mins = cal.get(Calendar.MINUTE);
+
+            int style = AlertDialog.THEME_HOLO_DARK;
+            timePickerDialog = new TimePickerDialog(this, style, timeSetListener, hrs, mins, true);
+
+
+        }
+    }
+
+
