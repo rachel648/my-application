cf62b0870d2d1d7c6c83fa8fe1dedc1c6ab2c6ec
rachel648 <rachelraqelmurithi@gmail.com>
2024-09-25 16:50:53 +0300

    lol

--- app/src/main/java/com/example/yogademoapp/Not.java
+++ app/src/main/java/com/example/yogademoapp/Not.java
@@ -174,7 +174,7 @@ public class Not extends AppCompatActivity {
                 })
                 .show();
     }
-
+//
     // Function to open Google Calendar
     private void openGoogleCalendar() {
         Intent intent = new Intent(Intent.ACTION_INSERT);
