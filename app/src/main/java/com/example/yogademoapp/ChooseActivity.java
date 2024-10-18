832c15cdbb44c5d08d50c40dde72f2371277502e
rachel648 <rachelraqelmurithi@gmail.com>
2024-10-18 18:11:01 +0300

    p

--- app/src/main/java/com/example/yogademoapp/ChooseActivity.java
+++ app/src/main/java/com/example/yogademoapp/ChooseActivity.java
@@ -2,13 +2,11 @@ package com.example.yogademoapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -65,9 +63,9 @@ public class ChooseActivity extends AppCompatActivity {
 
 
         // Set a listener for item selection in BottomNavigationView
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+      /*    bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+          public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.menu_home:
                         startActivity(new Intent(ChooseActivity.this, ChooseActivity.class));
@@ -88,7 +86,7 @@ public class ChooseActivity extends AppCompatActivity {
                         return false;
                 }
             }
-        });
+        });     */
 
     }
 
--- app/src/main/java/com/example/yogademoapp/mentaldashboard.java
+++ app/src/main/java/com/example/yogademoapp/mentaldashboard.java
@@ -9,7 +9,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -50,7 +49,7 @@ public class mentaldashboard extends AppCompatActivity {
         drawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+   /*    navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
@@ -80,7 +79,7 @@ public class mentaldashboard extends AppCompatActivity {
                 }
                 return true;
             }
-        });
+        }); */
 
         // Set the first Spinner initially to GONE
         spinner.setVisibility(View.GONE);
